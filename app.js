eee9b5018b5e1c36ad50d30dcfe2de586df43daf
Abiodun Adekunle Abdul-Jeleel <abduljeleelabiodun44@gmail.com>
2023-07-26 12:03:29 +0100

    Restructured form design

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ getSubmitBtn.addEventListener("click", () => {
   confirm("You're about to  submit!");
   alert("Congratulations! You've successfully registered!");
 });
+
+fetch("https://random-data-api.com/api/v2/users")
+  .then((message) => console.log(message.json()))
+  .then((err) => console.log(err));
